601adde8519f92cec04ea31682ca7dff0032fb1d
Eugenio <Ponterosso.e@gmail.com>
2022-08-02 10:06:21 +0100

    create basic file structure, create script and style
    create basic grid setup

---
+++ Scripts/app.js
@@ -0,0 +1,13 @@
+//Variables
+let gridSize = 16;
+let tileSize = 500 / gridSize;
+console.log(tileSize);
+//ui
+const gridContainer = document.querySelector('.container');
+//create div grid
+for (let i = 1; i <= gridSize * gridSize; ++i) {
+    let gridDiv = document.createElement('div');
+    gridDiv.id = 'gridDiv'+i;
+    gridDiv.className ='gridDiv';
+    gridContainer.appendChild(gridDiv);
+}
